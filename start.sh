a9177b04c4d38716817dfb99ecc52bb4b6479568
Ghazal-helall <2020001@std.aum.edu.jo>
2025-05-04 20:47:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-unzip -o Roberta_model.zip
-uvicorn main:app --host 0.0.0.0 --port 10000
+
+echo "Downloading model from Hugging Face..."
+wget https://huggingface.co/GhazalHelal/RoBERTa/resolve/main/Roberta_model.zip
+
+echo "Unzipping model..."
+unzip Roberta_model.zip
+
+echo "Starting server..."
+uvicorn main:app --host 0.0.0.0 --port $PORT
